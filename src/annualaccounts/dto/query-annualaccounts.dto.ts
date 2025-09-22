09644f6cbff7c05fdf5f149f40fc33d8fd26ce6f
Navinder Singh <navinder.singh@janaagraha.org>
2025-09-22 17:10:35 +0530

    Add Swagger.

--- src/annualaccounts/dto/query-annualaccounts.dto.ts
+++ src/annualaccounts/dto/query-annualaccounts.dto.ts
@@ -1,10 +1,52 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class QueryAnnualAccountsDto {
-  @IsOptional() @IsString() ulb?: string;
-  @IsOptional() @IsString() state?: string;
-  @IsOptional() @IsString() ulbType?: string;
-  @IsOptional() @IsString() popCat?: string;
-  @IsOptional() @IsString() year?: string;
-  @IsOptional() @IsString() auditType?: string;
+  @ApiPropertyOptional({
+    description: 'ULB ObjectId',
+    example: '5dd006d4ffbcc50cfd92c87c',
+  })
+  @IsOptional()
+  @IsString()
+  ulb?: string;
+
+  @ApiPropertyOptional({
+    description: 'State ObjectId',
+    example: '5dcf9d7316a06aed41c748ec',
+  })
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @ApiPropertyOptional({
+    description: 'ULB Type ObjectId',
+    example: '5dcfa67543263a0e75c71697',
+  })
+  @IsOptional()
+  @IsString()
+  ulbType?: string;
+
+  @ApiPropertyOptional({
+    description: 'Population category',
+    example: '500K-1M',
+  })
+  @IsOptional()
+  @IsString()
+  popCat?: string;
+
+  @ApiPropertyOptional({
+    description: 'Audit yearId',
+    example: '606aaf854dff55e6c075d219',
+  })
+  @IsOptional()
+  @IsString()
+  year?: string;
+
+  @ApiPropertyOptional({
+    description: 'Audit Type must be audited or unaudited',
+    example: 'audited',
+  })
+  @IsOptional()
+  @IsString()
+  auditType?: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('City Finance APIs')
+    .setDescription('V2 documentation.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
