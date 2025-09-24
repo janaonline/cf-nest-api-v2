76d75d40bf49dd791cce583a978b6523bf300726
Navinder Singh <navinder.singh@janaagraha.org>
2025-09-24 15:07:16 +0530

    Remove commented code.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // Create the main NestJS application instance using the root AppModule
@@ -63,24 +63,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-// TODO - To be removed.
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   const swaggerConfig = new DocumentBuilder()
-//     .setTitle('City Finance APIs')
-//     .setDescription('V2 documentation.')
-//     .setVersion('1.0')
-//     .build();
-//   const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
-//   SwaggerModule.setup('api', app, documentFactory);
-
-//   const configService = app.get(ConfigService);
-//   const port = configService.get<number>('PORT') || 3000;
-
-//   await app.listen(port);
-
-//   console.log(`🚀 Server running on http://localhost:${port}`);
-// }
-// bootstrap();
