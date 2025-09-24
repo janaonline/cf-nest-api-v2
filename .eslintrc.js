0d0e1e5c82a010563c5474f0bf67ff050a86bb68
jeevad <jeevananthamcse@gmail.com>
2025-09-24 16:29:40 +0530

    Prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended' // Integrates Prettier
+    ],
+    rules: {
+        'prettier/prettier': ['error', { endOfLine: 'lf', printWidth: 120 }],
+        'max-len': [
+            'error',
+            {
+                code: 120,
+                ignoreUrls: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+                ignoreComments: false
+            }
+        ]
+    },
+    ignorePatterns: ['dist/', 'node_modules/']
+};
