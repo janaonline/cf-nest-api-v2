c20c5b6a82293f68e3dbdf53d2b91d646d9bd0ea
jeevad <jeevananthamcse@gmail.com>
2025-09-25 12:02:22 +0530

    Ecosystem file updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,30 @@ module.exports = {
     {
       name: 'cf-nest-api-v2',
       script: 'dist/src/main.js',
-      // watch: true, // optional for dev
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      //   env_production: {
+      //     NODE_ENV: 'production',
+      //     PORT: 3000,
+      //   },
+    },
+    {
+      name: 'dev-cf-nest-api-v2',
+      script: 'dist/src/main.js',
       env: {
         NODE_ENV: 'development',
         PORT: 3000,
       },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3000,
+    },
+    {
+      name: 'stag-cf-nest-api-v2',
+      script: 'dist/src/main.js',
+      // watch: true, // optional for dev
+      env: {
+        NODE_ENV: 'staging',
+        PORT: 3001,
       },
     },
   ],
